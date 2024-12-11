549114be39851c560913d98c2ad25f6ef7dd8d53
MeloSCR <102454127+devmeloscr@users.noreply.github.com>
2024-12-11 11:12:40 -0300

    add welcome and steps components in first lesson

---
+++ src/components/steps/index.tsx
@@ -0,0 +1,34 @@
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+import { Step } from "../step";
+import { IconMapPin, IconQrcode, IconTicket } from "@tabler/icons-react-native";
+
+export function Steps() {
+    const stepsArray = [
+        {
+            icon: IconMapPin,
+            title: "Encontre estabelecimentos",
+            description: "Veja locais perto de você que são parceiros Nearby",
+        },
+        {
+            icon: IconQrcode,
+            title: "Ative o cupom com QR Code",
+            description: "Escaneie o código no estabelecimento para usar o benefício"
+        },
+        {
+            icon: IconTicket,
+            title: "Garanta vantagens perto de você",
+            description: "Ative cupons onde estiver, em diferentes tipos de estabelecimento "
+        }
+    ]
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Veja como funciona:</Text>
+            {
+                stepsArray.map((step, index) => (
+                    <Step key={index} icon={step.icon} title={step.title} description={step.description} />
+                ))
+            }
+        </View>
+    )
+}
